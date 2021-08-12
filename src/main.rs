d2d374d405940e8178ff0bdc060ac359f6d2d7ef
Sönke Hahn <soenkehahn@gmail.com>
2021-08-12 14:21:31 -0400

    Make work for projects other than agora, no workspace support

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use anyhow::*;
 use cargo_metadata::MetadataCommand;
 use cradle::prelude::*;
+use question::{Answer, Question};
 use std::env;
 use structopt::StructOpt;
 use tempfile::tempdir;
@@ -7,24 +9,27 @@ use tempfile::tempdir;
 #[derive(StructOpt)]
 struct Arguments {
   revision: String,
-  #[structopt(long)]
-  publish_agora_lnd_client: bool,
 }
 
-fn main() {
+fn main() -> Result<()> {
   let arguments = Arguments::from_args();
-
-  let tempdir = tempdir().unwrap();
-
+  let tempdir = tempdir()?;
+  let StdoutTrimmed(repo_url) = cmd!(%"git remote get-url origin");
+  let current_dir = env::current_dir()?;
+  let crate_name = current_dir
+    .file_name()
+    .ok_or(anyhow!("current dir has no file name"))?
+    .to_str()
+    .ok_or(anyhow!("current dir not valid utf-8"))?;
   (
+    CurrentDir(tempdir.path()),
     "git",
     "clone",
-    "git@github.com:agora-org/agora.git",
-    CurrentDir(tempdir.path()),
+    &repo_url,
+    crate_name,
   )
     .run_unit();
-
-  env::set_current_dir(tempdir.path().join("agora")).unwrap();
+  env::set_current_dir(tempdir.path().join(crate_name))?;
 
   (
     "git",
@@ -34,46 +39,38 @@ fn main() {
     "master",
   )
     .run_unit();
+  ("git", "checkout", &arguments.revision).run_unit();
 
-  ("git", "checkout", arguments.revision).run_unit();
+  ("cargo", "publish", "--dry-run").run_unit();
 
-  let metadata = MetadataCommand::new().exec().unwrap();
+  let metadata = MetadataCommand::new().exec()?;
 
   let version = metadata
     .packages
     .into_iter()
-    .filter(|package| package.name == "agora")
+    .filter(|package| package.name == crate_name)
     .next()
-    .unwrap()
+    .ok_or(anyhow!(
+      "package '{}' not found in cargo metadata",
+      crate_name
+    ))?
     .version;
 
-  if arguments.publish_agora_lnd_client {
-    (
-      "cargo",
-      "publish",
-      "--dry-run",
-      CurrentDir("agora-lnd-client"),
-    )
-      .run_unit();
+  let tag_name = format!("v{}", version);
+
+  eprintln!("crate name: {}", crate_name);
+  eprintln!("repo: {}", repo_url);
+  eprintln!("revision: {} (merged into master)", arguments.revision);
+  eprintln!("tag: {}", tag_name);
+  let answer = Question::new("Continue?")
+    .default(Answer::NO)
+    .show_defaults()
+    .confirm();
+
+  if let Answer::YES = answer {
+    ("git", "tag", tag_name).run_unit();
+    ("git", "push", "origin", &version.to_string()).run_unit();
+    ("cargo", "publish").run_unit();
   }
-
-  ("cargo", "publish", "--dry-run").run_unit();
-
-  (
-    "git",
-    "tag",
-    "--sign",
-    "--message",
-    format!("Release version {}", version),
-    version.to_string(),
-  )
-    .run_unit();
-
-  ("git", "push", "origin", &version.to_string()).run_unit();
-
-  if arguments.publish_agora_lnd_client {
-    ("cargo", "publish", CurrentDir("agora-lnd-client")).run_unit();
-  }
-
-  ("cargo", "publish").run_unit();
+  Ok(())
 }
